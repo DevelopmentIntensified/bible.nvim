a9bd776d3a624f306191b6b8b57fad74a03339d2
developmentIntensified <developmentIntensified@gmail.com>
2025-01-06 22:52:15 -0500

    fix for linux

--- lua/telescope/_extensions/bible/config.lua
+++ lua/telescope/_extensions/bible/config.lua
@@ -6,6 +6,6 @@ local configDir = vim.fn.fnamemodify(configPath, ":h")
 return {
 	home = home,
 	-- plugin_dir = debug.getinfo(1).source:sub(2),
-	code_dir = configDir .. "\\",
-	cache_dir = vim.fn.stdpath("data") .. "\\bible.nvim\\",
+	code_dir = configDir .. "/",
+	cache_dir = vim.fn.stdpath("data") .. "/bible.nvim/",
 }
